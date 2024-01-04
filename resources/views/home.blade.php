975f5e0df457de641d2347a8c17cec4121701436
perkoooo <106542058+perkoooo@users.noreply.github.com>
2024-01-04 11:55:16 -0300

    Add files via upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('plugins.Sweetalert2', true)
+
+@section('content_header')
+    <h1>La concha de tu madre carlitos</h1>
+@stop
+
+@section('content')
+    <p>Ola</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
